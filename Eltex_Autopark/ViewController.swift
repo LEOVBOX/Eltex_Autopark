ad611ae66dff3ff410412a076338abd29610bd74
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2024-10-01 22:36:58 +0700

    Initial Commit

---
+++ Eltex_Autopark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eltex_Autopark
+//
+//  Created by Леонид Шайхутдинов on 01.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
